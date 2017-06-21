d7bace58872a86a366d893d16171545d9c20a62b
gda2016 <gda10672@gmail.com>
2017-06-21 10:17:02 +0300

    Step 3 : 3  Add view About and appropriate route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
+// Route::get('/hello', function () {
+//     return '<h1>Hello World</h1>';
+// });
+
+Route::get('/about', function(){
+	return view('pages.about');
 });
